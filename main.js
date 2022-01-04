52979676a852c987ceeeabcea9d513461c136aaf
Samosathree <84139052+Samosathree@users.noreply.github.com>
2022-01-04 11:56:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "WHYYYY ";
+var last_name = "MEEEE?!?!";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10000000000;
+console.log(a)
+var b = 1
+console.log(b)
+var add = a + b;
+console.log(add);
